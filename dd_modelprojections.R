149c6ae2a8c674117a9f3591e354f7fc234569d4
Lauren Hallett <hallett@uoregon.edu>
2018-12-05 12:26:58 -0700

    standardize upper lambda values

--- dd_modelprojections.R
+++ dd_modelprojections.R
@@ -40,7 +40,7 @@ eg <- .6
 #m1 <- as.formula(log(ERseedout +1) ~  log(eg*(ERseedin+1)*exp(log(lambda/(1+aiE*(ERseedin+1)*eg+aiA*(AVseedin+1)*ag)))))
 
 # same as above, just different formalization
-m1 <- as.formula(log(ERseedout +1) ~  log(eg*(ERseedin+1)*exp(log(lambda)-log((1+aiE*(ERseedin+1)*eg+aiA*(AVseedin+1)*ag)))))
+m1E <- as.formula(log(ERseedout +1) ~  log(eg*(ERseedin+1)*exp(log(lambda)-log((1+aiE*(ERseedin+1)*eg+aiA*(AVseedin+1)*ag)))))
 
 # old m1 version
 #m1 <- as.formula(log(ERseedout +1) ~  log((ERseedin*eg +1))*((lambda)/(1+aiE*log((ERseedin*eg + 1)) + aiA*log(AVseedin*ag + 1))))
@@ -50,8 +50,8 @@ treatments <- unique(togdat$treatment)
 ERoutput <- as.data.frame(matrix(nrow = 0, ncol = 7))
 names(ERoutput) = c("estimate", "se", "t", "p", "params", "treatment", "species")
 for (i in 1:length(treatments)){
-  m1out <- nlsLM(m1, start=list(lambda=1, aiE = .01, aiA=.01),
-                 lower = c(0, 0, 0), upper = c(30, 1, 1),
+  m1out <- nlsLM(m1E, start=list(lambda=1, aiE = .01, aiA=.01),
+                 lower = c(0, 0, 0), upper = c(200, 1, 1),
                  control=nls.lm.control(maxiter=500), trace=T,
                  data = subset(dat, !is.na(ERseedout) & treatment == treatments[i]))
   
@@ -68,7 +68,7 @@ for (i in 1:length(treatments)){
 ## With seed bank 
 
 # new model, log transformed
-m1 <- as.formula(log(AVseedout +1) ~  log(ag*(AVseedin+1)*exp(log(lambda)-log((1+aiE*(ERseedin+1)*eg+aiA*(AVseedin+1)*ag)))))
+m1A <- as.formula(log(AVseedout +1) ~  log(ag*(AVseedin+1)*exp(log(lambda)-log((1+aiE*(ERseedin+1)*eg+aiA*(AVseedin+1)*ag)))))
 
 
 # old model
@@ -80,9 +80,9 @@ AVoutput <- as.data.frame(matrix(nrow = 0, ncol = 7))
 names(AVoutput) = c("estimate", "se", "t", "p", "params", "treatment", "species")
 for (i in 1:length(treatments)){
   
-  m1out <- nlsLM(m1, start=list(lambda=1, aiE = .01, aiA=.01), 
+  m1out <- nlsLM(m1A, start=list(lambda=1, aiE = .01, aiA=.01), 
                  control=nls.lm.control(maxiter=500), 
-                 lower = c(0, 0, 0), upper = c(100, 1, 1),
+                 lower = c(0, 0, 0), upper = c(200, 1, 1),
                  trace=T,
                  data = subset(dat, !is.na(AVseedout) & treatment == treatments[i] & rm == 0))
   outreport <- as.data.frame(summary(m1out)$coef[1:3, 1:4])
@@ -97,8 +97,7 @@ for (i in 1:length(treatments)){
 model.dat <- rbind(ERoutput, AVoutput) %>%
   tbl_df() %>%
   dplyr::select(estimate, params, treatment, species) %>%
-  spread(params, estimate)
-
+  spread(params, estimate) 
 
 # ggplot(model.dat, aes(x=treatment, y=aiA)) + geom_bar(stat="identity") + facet_wrap(~species)
 # ggplot(model.dat, aes(x=treatment, y=aiE)) + geom_bar(stat="identity") + facet_wrap(~species)
@@ -113,9 +112,6 @@ parameter_table <- rbind(ERoutput, AVoutput) %>%
   dplyr::select(estimateout, params, treatment, species) %>%
   spread(params, estimateout) %>%
   select(treatment, species, lambda, aiA, aiE) %>%
-  mutate(lambda = ifelse(treatment == "fallDry" & species == "Erodium", "7.41 ± 3.64", lambda),
-         aiE = ifelse(treatment == "fallDry" & species == "Erodium", "0.000063 ± 0.0088", aiE),
-         aiA = ifelse(treatment == "fallDry" & species == "Erodium", "0.0096 ± 0.013", aiA)) %>%
   arrange(species, treatment)
 
 
