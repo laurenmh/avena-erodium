afb1594fa6a5aeebb1b1c9a8d432d37f5a6afe53
Lauren Hallett <hallett@uoregon.edu>
2018-12-05 11:46:46 -0700

    model fit updates

--- dd_modelfits.R
+++ dd_modelfits.R
@@ -43,11 +43,12 @@ eg <- .6
 # model
 m1 <- as.formula(log(ERseedout +1) ~  log(eg*(ERseedin+1)*exp(log(lambda)-log((1+aiE*(ERseedin+1)*eg+aiA*(AVseedin+1)*ag)))))
 
-set.seed(100)
+
 m1out <- nlsLM(m1, start=list(lambda=1, aiE = .01, aiA=.01),
                  lower = c(0, 0, 0), upper = c(30, 1, 1),
                  control=nls.lm.control(maxiter=500), trace=T,
                  data = subset(dat, !is.na(ERseedout) & treatment == "fallDry"))
+
 
 
 m1out_big <- nlsLM(m1, start=list(lambda=158, aiE = .01, aiA=.01),
@@ -58,3 +59,29 @@ summary(m1out_big)
 logLik(m1out_big)
 
 lrtest( m1out_big, m1out)
+
+# ## attempts at nls_multstart 
+# library(nls.multstart)
+# 
+# growthfunc <- function(lambda, aiE, aiA, ERseedin, AVseedin){
+#   as <- .4
+#   ag <- .9
+#   es <- .82
+#   eg <- .6
+#   newcol <- log(eg*(ERseedin+1)*exp(log(lambda)-log((1+aiE*(ERseedin+1)*eg+aiA*(AVseedin+1)*ag))))
+#   return(newcol)
+# }
+# 
+# 
+# dat$newcol <- log(dat$ERseedout + 1)
+# subdat <- subset(dat, !is.na(ERseedout) & treatment == "fallDry")
+# 
+# a <- nls_multstart(newcol ~ growthfunc(lambda, aiE, aiA, ERseedin, AVseedin), 
+#                    data = subdat,
+#                    iter = 500,
+#                    start_lower=c(lambda=0, aiE = 0, aiA=0),
+#                    start_upper = c(lambda=200, aiE = 1, aiA=1),
+#                    lower = c(lambda=0, aiE = 0, aiA=0),
+#                    upper = c(lambda=200, aiE =2, aiA=1))
+# summary(a)
+# 
