d1d738f7e24e19f9f7a92c036c14f6e9362c996d
Lauren Hallett <hallett@uoregon.edu>
2018-12-06 10:16:58 -0700

    fig 2s

--- FigS2.R
+++ FigS2.R
@@ -8,10 +8,24 @@ consistent.out2 <- consistent.out %>%
 
 ## Simulation graph ##
 #pdf("Consistentsimulation.pdf", width = 12, height = 6)
-ggplot(consistent.out2, aes(x=time, y=(count), color = species)) + geom_line(size = 4) + facet_grid(invader ~ treatment) + 
-  theme_bw() +  theme(text = element_text(size = 24), legend.position = "none", strip.background = element_blank(),
-                      #strip.text.x = element_text(size = 16), strip.text.y = element_text(size = 16),
-                      panel.grid.major = element_blank(), panel.grid.minor = element_blank()) + scale_y_log10(limits=c(.1, 1200), breaks = c( 1, 10, 100, 1000)) +
+p <- ggplot(consistent.out2, aes(x=time, y=(count), color = species)) + geom_line(size = 2) + 
+  facet_grid(invader ~ treatment) + 
+  theme_bw() +  
+  theme(strip.background = element_blank(), legend.position = "none",text = element_text(size = 16), 
+        strip.text.x = element_text(size = 16), strip.text.y = element_text(size = 16),
+        panel.grid.major = element_blank(), panel.grid.minor = element_blank()) + 
+  scale_y_log10(limits=c(.1, 3000), breaks = c( 1, 10, 100, 1000)) +
   labs(y=expression(paste("Count (individuals/m"^"2",")")), x = "Time step") + scale_color_manual(values = c("grey80", "grey30"))
-ggsave(here("Figs", "fig3_consistent-simulation.pdf"), width = 12, height = 8)
-ggsave(here("Figs", "fig3_consistent-simulation.jpg"), width = 12, height = 8)
+#ggsave(here("Figs", "fig3_consistent-simulation.pdf"), width = 12, height = 8)
+#ggsave(here("Figs", "fig3_consistent-simulation.jpg"), width = 12, height = 8)
+
+
+
+#Use grid.text
+
+pdf(here("Figs", "fig2s.pdf"), width = 8, height = 5)
+p
+grid.text(c("(a)", "(b)", "(c)", "(d)", "(e)", "(f)", "(g)", "(h)"),x = c(0.29,0.5,.715,.925 ),
+          y = c(rep(.89, 4), rep(.48, 4)),
+          gp=gpar(fontsize=16))
+dev.off()
